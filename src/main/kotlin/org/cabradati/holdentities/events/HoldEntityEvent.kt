d11200bf53a56cc809d4d0521f9a54b3b774f33c
Erick B. Lemos <ericklemoscosta@gmail.com>
2022-11-23 12:20:31 -0300

    melhoria em codigo

--- src/main/kotlin/org/cabradati/holdentities/events/HoldEntityEvent.kt
+++ src/main/kotlin/org/cabradati/holdentities/events/HoldEntityEvent.kt
@@ -1,12 +1,12 @@
 package org.cabradati.holdentities.events
 
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.cabradati.holdentities.App
 import org.cabradati.holdentities.DIContainer
+import org.cabradati.holdentities.utils.extensions.canHold
 
 class HoldEntityEvent(
     private val diContainer: DIContainer
@@ -16,17 +16,13 @@ class HoldEntityEvent(
     fun onHoldEntity(event: PlayerInteractEntityEvent) {
 
         val toggleDaEntidade = diContainer.config.getBoolean(App.PREFIXO_ENTIDADE_SEGURAVEL + event.rightClicked.type)
-        if (!(isHoldable(event.player) && toggleDaEntidade)) return
+        if (!(event.player.canHold() && toggleDaEntidade)) return
 
         val player = event.player
         val entity = event.rightClicked
+
         player.addPassenger(entity)
 
-    }
-
-    private fun isHoldable(player: Player): Boolean {
-        return player.passengers.isEmpty() &&
-                player.equipment.itemInMainHand.amount == 0
     }
 
 }
